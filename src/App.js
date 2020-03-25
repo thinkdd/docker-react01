87a9b8ef67b25eb6bfaa293dcae33f39c7ac7e40
Wirat Chunchob <Wiratc@nu.ac.th>
2020-03-25 15:15:46 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>Hello World three times</p>
+          <p>Make Changes</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React with Docker 
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+
+export default App;
